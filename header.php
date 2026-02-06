921e20488404e1c58db797caf83915cfd12839ca
Luis Carlos Pando <hey@luiscarlospando.com>
2026-02-06 15:40:28 -0600

    Dinamizar navmenu

--- header.php
+++ header.php
@@ -172,7 +172,7 @@
                         <div class="col-6 col-md-3">
                             <a class="rounded-pill" href="#" target="_blank">
                                 <i class="fa-solid fa-hand-holding-heart"></i>
-                                Quiero ayudar
+                                ¡Quiero ayudar!
                             </a>
                         </div>
                     </div>
