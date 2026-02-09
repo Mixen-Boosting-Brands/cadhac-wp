efc7f966f435e6ceac4b39a38bbfe0bdbd5441c2
Luis Carlos Pando <hey@luiscarlospando.com>
2026-02-09 17:15:24 -0600

    Agregar template prensa

---
+++ page-prensa.php
@@ -0,0 +1,307 @@
+<?php
+/*
+Template Name: Prensa
+*/
+get_header(); ?>
+
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1
+                    class="lh-1"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                >
+                    Sala
+                    <span>de</span><br />
+                    prensa
+                </h1>
+                <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"
+                >
+                    Información clara y verificada sobre derechos
+                    humanos en Nuevo León, desde una relación
+                    responsable con los medios de comunicación para
+                    fortalecer la memoria, la verdad y la justicia.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div
+                    class="arrow-right home-image container-image bp-center rounded-5 p-4"
+                    style="
+                        background-image: url(&quot;<?php echo esc_url(
+                            get_template_directory_uri(),
+                        ); ?>/assets/images/bg-j-9.png&quot;);
+                    "
+                    data-aos="fade-up"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="200"
+                ></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-60 pb-30">
+    <div class="container-fluid">
+        <div class="row mb-4">
+            <div class="col-12">
+                <h1 data-aos="fade-up" data-aos-duration="1000">
+                    Press <span>kit</span><br />
+                    CADHAC
+                </h1>
+                <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"
+                >
+                    Material institucional disponible para medios de
+                    comunicación, con información verificada, recursos
+                    visuales y documentos oficiales para su correcta
+                    difusión.
+                </p>
+            </div>
+        </div>
+        <div
+            class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-3 mb-5"
+        >
+            <div class="col">
+                <div
+                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"
+                >
+                    <div>
+                        <img
+                            src="<?php echo esc_url(
+                                get_template_directory_uri(),
+                            ); ?>/assets/images/prensa/ico-1@2x.png"
+                            class="ico"
+                            alt=""
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                <strong>Fotografías autorizadas:</strong
+                                ><br />
+                                (Imágenes de archivo y uso editorial)
+                            </p>
+                            <a
+                                class="btn btn-primary rounded-pill"
+                                href="#"
+                                download
+                            >
+                                <i
+                                    class="fa-solid fa-circle-arrow-down"
+                                ></i>
+                                Descargar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div
+                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="200"
+                >
+                    <div>
+                        <img
+                            src="<?php echo esc_url(
+                                get_template_directory_uri(),
+                            ); ?>/assets/images/prensa/ico-2@2x.png"
+                            class="ico"
+                            alt=""
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                <strong>Biografia institucional:</strong
+                                ><br />
+                                CADHAC y Hermana Consuelo Morales.
+                            </p>
+                            <a
+                                class="btn btn-primary rounded-pill"
+                                href="#"
+                                download
+                            >
+                                <i
+                                    class="fa-solid fa-circle-arrow-down"
+                                ></i>
+                                Descargar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div
+                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="300"
+                >
+                    <div>
+                        <img
+                            src="<?php echo esc_url(
+                                get_template_directory_uri(),
+                            ); ?>/assets/images/prensa/ico-3@2x.png"
+                            class="ico"
+                            alt=""
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                <strong>Perfil Institucional:</strong
+                                ><br />
+                                CADHAC y su trabajo en derechos humanos
+                                en Nuevo León.
+                            </p>
+                            <a
+                                class="btn btn-primary rounded-pill"
+                                href="#"
+                                download
+                            >
+                                <i
+                                    class="fa-solid fa-circle-arrow-down"
+                                ></i>
+                                Descargar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div
+                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="400"
+                >
+                    <div>
+                        <img
+                            src="<?php echo esc_url(
+                                get_template_directory_uri(),
+                            ); ?>/assets/images/prensa/ico-4@2x.png"
+                            class="ico"
+                            alt=""
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                <strong>Línea del tiempo:</strong><br />
+                                Hitos clave de la organización.
+                            </p>
+                            <a
+                                class="btn btn-primary rounded-pill"
+                                href="#"
+                                download
+                            >
+                                <i
+                                    class="fa-solid fa-circle-arrow-down"
+                                ></i>
+                                Descargar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div
+                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="500"
+                >
+                    <div>
+                        <img
+                            src="<?php echo esc_url(
+                                get_template_directory_uri(),
+                            ); ?>/assets/images/prensa/ico-5@2x.png"
+                            class="ico"
+                            alt=""
+                        />
+                        <div class="card-body">
+                            <p class="card-text">
+                                <strong
+                                    >Publicaciones
+                                    institucionales:</strong
+                                ><br />
+                                (25 años, memorias, informes clave)
+                            </p>
+                            <a
+                                class="btn btn-primary rounded-pill"
+                                href="#"
+                                download
+                            >
+                                <i
+                                    class="fa-solid fa-circle-arrow-down"
+                                ></i>
+                                Descargar
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <a class="btn btn-primary btn-lg rounded-pill" href="#"
+                    >Ver más comunicados</a
+                >
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-60 pb-30">
+    <div class="container-fluid">
+        <div class="row mb-4">
+            <div class="col-12">
+                <h1
+                    class="lh-1"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                >
+                    Contacto <span>para</span><br />
+                    medios
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"
+                >
+                    Para solicitudes de entrevista, información
+                    adicional o aclaraciones, favor de contactar a:
+                </p>
+                <?php get_template_part(
+                    "template-parts/contact/card-contact",
+                ); ?>
+            </div>
+            <div class="col-lg-6">
+                <div
+                    class="arrow-right home-image container-image bp-center rounded-5 p-4"
+                    style="
+                        background-image: url(&quot;<?php echo esc_url(
+                            get_template_directory_uri(),
+                        ); ?>/assets/images/bg-prensa.png&quot;);
+                    "
+                    data-aos="fade-up"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                ></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
