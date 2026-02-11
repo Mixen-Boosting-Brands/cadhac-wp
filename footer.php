b28c4dc9e71285930893e14f19f344e922938813
Luis Carlos Pando <hey@luiscarlospando.com>
2026-02-10 19:01:11 -0600

    Debuggear

--- footer.php
+++ footer.php
@@ -1,270 +1,168 @@
 		<?php if (is_home()): ?>
             <?php
             $pagina_home = get_field("pagina_home", "option");
-
             $tarjetas = $pagina_home["tarjetas"] ?? null;
-
-            if (!$tarjetas) {
-                return;
-            }
-
-            $texto_superior = $tarjetas["texto_superior"] ?? "";
-            $texto_inferior = $tarjetas["texto_inferior"] ?? "";
-
-            $t1 = $tarjetas["tarjeta_1"] ?? null;
-            $t2 = $tarjetas["tarjeta_2"] ?? null;
-            $t3 = $tarjetas["tarjeta_3"] ?? null;
-            $t4 = $tarjetas["tarjeta_4"] ?? null;
             ?>
 
-            <section class="pt-30 pb-60">
-                <div class="container-fluid">
-                    <!-- Intro -->
-                    <div class="row mb-4">
-                        <div class="col-12">
+            <?php if ($tarjetas): ?>
 
-                            <?php if (!empty($texto_superior)): ?>
-                                <h1
-                                    data-aos="fade-up"
-                                    data-aos-duration="1000"
-                                >
-                                    <?php echo wp_kses_post($texto_superior); ?>
-                                </h1>
-                            <?php endif; ?>
+                <?php
+                $texto_superior = $tarjetas["texto_superior"] ?? "";
+                $texto_inferior = $tarjetas["texto_inferior"] ?? "";
 
-                            <?php if (!empty($texto_inferior)): ?>
-                                <div
-                                    data-aos="fade-up"
-                                    data-aos-duration="1000"
-                                    data-aos-delay="100"
-                                >
-                                    <?php echo wp_kses_post($texto_inferior); ?>
-                                </div>
-                            <?php endif; ?>
+                $t1 = $tarjetas["tarjeta_1"] ?? null;
+                $t2 = $tarjetas["tarjeta_2"] ?? null;
+                $t3 = $tarjetas["tarjeta_3"] ?? null;
+                $t4 = $tarjetas["tarjeta_4"] ?? null;
+                ?>
 
-                        </div>
-                    </div>
+                <section class="pt-30 pb-60">
+                    <div class="container-fluid">
 
-                    <div class="row mb-5">
-                        <!-- Tarjeta 1 -->
-                        <?php if ($t1): ?>
-                            <div class="col-md-6 col-lg-3">
-                                <div
-                                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
-                                    data-aos="fade-up"
-                                    data-aos-duration="1000"
-                                    data-aos-delay="100"
-                                >
-                                    <div>
+                        <!-- Intro -->
+                        <div class="row mb-4">
+                            <div class="col-12">
 
-                                        <?php if (!empty($t1["icono"])): ?>
-                                            <img
-                                                src="<?php echo esc_url(
-                                                    $t1["icono"],
-                                                ); ?>"
-                                                class="ico"
-                                                alt=""
-                                                loading="lazy"
-                                                decoding="async"
-                                            />
-                                        <?php endif; ?>
+                                <?php if (!empty($texto_superior)): ?>
+                                    <h1 data-aos="fade-up" data-aos-duration="1000">
+                                        <?php echo wp_kses_post(
+                                            $texto_superior,
+                                        ); ?>
+                                    </h1>
+                                <?php endif; ?>
 
-                                        <?php if (!empty($t1["texto"])): ?>
-                                            <div class="card-body">
-                                                <p class="card-text">
-                                                    <?php echo esc_html(
-                                                        $t1["texto"],
-                                                    ); ?>
-                                                </p>
-                                            </div>
-                                        <?php endif; ?>
-
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endif; ?>
-
-                        <!-- Tarjeta 2 -->
-                        <?php if ($t2): ?>
-                            <div class="col-md-6 col-lg-3">
-                                <div
-                                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
-                                    data-aos="fade-up"
-                                    data-aos-duration="1000"
-                                    data-aos-delay="200"
-                                >
-                                    <div>
-
-                                        <?php if (!empty($t2["icono"])): ?>
-                                            <img
-                                                src="<?php echo esc_url(
-                                                    $t2["icono"],
-                                                ); ?>"
-                                                class="ico"
-                                                alt=""
-                                                loading="lazy"
-                                                decoding="async"
-                                            />
-                                        <?php endif; ?>
-
-                                        <?php if (!empty($t2["texto"])): ?>
-                                            <div class="card-body">
-                                                <p class="card-text">
-                                                    <?php echo esc_html(
-                                                        $t2["texto"],
-                                                    ); ?>
-                                                </p>
-                                            </div>
-                                        <?php endif; ?>
-
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endif; ?>
-
-                        <!-- Tarjeta 3 -->
-                        <?php if ($t3): ?>
-                            <div class="col-md-6 col-lg-3">
-                                <div
-                                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
-                                    data-aos="fade-up"
-                                    data-aos-duration="1000"
-                                    data-aos-delay="300"
-                                >
-                                    <div>
-
-                                        <?php if (!empty($t3["icono"])): ?>
-                                            <img
-                                                src="<?php echo esc_url(
-                                                    $t3["icono"],
-                                                ); ?>"
-                                                class="ico"
-                                                alt=""
-                                                loading="lazy"
-                                                decoding="async"
-                                            />
-                                        <?php endif; ?>
-
-                                        <?php if (!empty($t3["texto"])): ?>
-                                            <div class="card-body">
-                                                <p class="card-text">
-                                                    <?php echo esc_html(
-                                                        $t3["texto"],
-                                                    ); ?>
-                                                </p>
-                                            </div>
-                                        <?php endif; ?>
-
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endif; ?>
-
-                        <!-- Tarjeta 4 -->
-                        <?php if ($t4): ?>
-                            <div class="col-md-6 col-lg-3">
-                                <div
-                                    class="card card-ico-center rounded-5 mb-4 mb-lg-0"
-                                    data-aos="fade-up"
-                                    data-aos-duration="1000"
-                                    data-aos-delay="400"
-                                >
-                                    <div>
-
-                                        <?php if (!empty($t4["icono"])): ?>
-                                            <img
-                                                src="<?php echo esc_url(
-                                                    $t4["icono"],
-                                                ); ?>"
-                                                class="ico"
-                                                alt=""
-                                                loading="lazy"
-                                                decoding="async"
-                                            />
-                                        <?php endif; ?>
-
-                                        <?php if (!empty($t4["texto"])): ?>
-                                            <div class="card-body">
-                                                <p class="card-text">
-                                                    <?php echo esc_html(
-                                                        $t4["texto"],
-                                                    ); ?>
-                                                </p>
-                                            </div>
-                                        <?php endif; ?>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endif; ?>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-12 text-end arrow-right">
-                            <a href="#">
-                                <h2>
-                                    Conoce nuestro trabajo
-                                    <i class="fa-solid fa-arrow-right-long"></i>
-                                </h2>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        <?php endif; ?>
-
-        <?php if (is_home()): ?>
-            <?php
-            $banner_amores = get_field("banner_amores", "option");
-
-            if (!$banner_amores) {
-                return;
-            }
-
-            $texto = $banner_amores["texto"] ?? "";
-            $imagen = $banner_amores["imagen"] ?? "";
-            ?>
-
-            <?php if (!empty($imagen)): ?>
-            <section
-                class="cta py-60"
-                style="background-image:url('<?php echo esc_url($imagen); ?>');"
-            >
-            <?php else: ?>
-            <section class="cta py-60">
-            <?php endif; ?>
-
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-
-                            <div class="w-100 arrow-right p-5 text-center">
-
-                                <!-- TEXTO -->
-                                <?php if (!empty($texto)): ?>
-                                    <div class="cta-text">
-                                        <?php echo wp_kses_post($texto); ?>
+                                <?php if (!empty($texto_inferior)): ?>
+                                    <div
+                                        data-aos="fade-up"
+                                        data-aos-duration="1000"
+                                        data-aos-delay="100"
+                                    >
+                                        <?php echo wp_kses_post(
+                                            $texto_inferior,
+                                        ); ?>
                                     </div>
                                 <?php endif; ?>
 
+                            </div>
+                        </div>
 
+                        <!-- Tarjetas -->
+                        <div class="row mb-5">
+                            <?php
+                            $cards = [$t1, $t2, $t3, $t4];
+                            $delay = 100;
+                            ?>
 
-                                <!-- LINK HARDCODED -->
-                                <a href="<?php echo esc_url(
-                                    get_permalink(2214),
-                                ); ?>">
+                            <?php foreach ($cards as $card): ?>
+                                <?php if ($card): ?>
+                                    <div class="col-md-6 col-lg-3">
+                                        <div
+                                            class="card card-ico-center rounded-5 mb-4 mb-lg-0"
+                                            data-aos="fade-up"
+                                            data-aos-duration="1000"
+                                            data-aos-delay="<?php echo esc_attr(
+                                                $delay,
+                                            ); ?>"
+                                        >
+                                            <div>
+
+                                                <?php if (
+                                                    !empty($card["icono"])
+                                                ): ?>
+                                                    <img
+                                                        src="<?php echo esc_url(
+                                                            $card["icono"],
+                                                        ); ?>"
+                                                        class="ico"
+                                                        alt=""
+                                                        loading="lazy"
+                                                        decoding="async"
+                                                    />
+                                                <?php endif; ?>
+
+                                                <?php if (
+                                                    !empty($card["texto"])
+                                                ): ?>
+                                                    <div class="card-body">
+                                                        <p class="card-text">
+                                                            <?php echo esc_html(
+                                                                $card["texto"],
+                                                            ); ?>
+                                                        </p>
+                                                    </div>
+                                                <?php endif; ?>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+
+                                <?php $delay += 100; ?>
+                            <?php endforeach; ?>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 text-end arrow-right">
+                                <a href="#">
                                     <h2>
-                                        Conoce más
+                                        Conoce nuestro trabajo
                                         <i class="fa-solid fa-arrow-right-long"></i>
                                     </h2>
                                 </a>
-
                             </div>
-
                         </div>
                     </div>
-                </div>
+                </section>
+            <?php endif; ?>
+        <?php endif; ?>
 
-            </section>
+        <?php if (is_home()): ?>
+            <?php $banner_amores = get_field("banner_amores", "option"); ?>
+
+            <?php if ($banner_amores): ?>
+
+                <?php
+                $texto = $banner_amores["texto"] ?? "";
+                $imagen = $banner_amores["imagen"] ?? "";
+                ?>
+
+                <?php if (!empty($imagen)): ?>
+                    <section
+                        class="cta py-60"
+                        style="background-image:url('<?php echo esc_url(
+                            $imagen,
+                        ); ?>');"
+                    >
+                <?php else: ?>
+                    <section class="cta py-60">
+                <?php endif; ?>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="w-100 arrow-right p-5 text-center">
+
+                                        <?php if (!empty($texto)): ?>
+                                            <div class="cta-text">
+                                                <?php echo wp_kses_post(
+                                                    $texto,
+                                                ); ?>
+                                            </div>
+                                        <?php endif; ?>
+
+                                        <a href="<?php echo esc_url(
+                                            get_permalink(2214),
+                                        ); ?>">
+                                            <h2>
+                                                Conoce más
+                                                <i class="fa-solid fa-arrow-right-long"></i>
+                                            </h2>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+            <?php endif; ?>
         <?php endif; ?>
 
         <?php get_template_part("template-parts/components/necesitas-ayuda"); ?>
