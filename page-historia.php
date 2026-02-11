edf1815aca6ea968245ab0e56a38beafc6a34324
Luis Carlos Pando <hey@luiscarlospando.com>
2026-02-11 16:11:50 -0600

    Dinamizar bloque 2

--- page-historia.php
+++ page-historia.php
@@ -8,168 +8,196 @@ get_header(); ?>
     "handle" => "pagina_historia",
 ]); ?>
 
+<?php
+$pagina_historia = get_field("pagina_historia", "option");
+
+if (!$pagina_historia) {
+    return;
+}
+
+/* =========================
+   BLOQUE 2
+========================= */
+
+$bloque_2 = $pagina_historia["bloque_2"] ?? null;
+
+$texto_superior = $bloque_2["texto_superior"] ?? "";
+$texto_inferior = $bloque_2["texto_inferior"] ?? "";
+$imagen_bloque = $bloque_2["imagen"] ?? "";
+
+/* =========================
+   MVV
+========================= */
+
+$mision = $pagina_historia["mision"] ?? null;
+$vision = $pagina_historia["vision"] ?? null;
+$valores = $pagina_historia["valores"] ?? null;
+?>
+
 <section class="pt-60 pb-30">
     <div class="container-fluid">
+
+        <!-- Intro -->
         <div class="row">
             <div class="col-lg-6">
-                <h1
-                    class="lh-1"
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                >
-                    El inicio de un<br />
-                    acompañamiento <span>colectivo.</span>
-                </h1>
-                <p
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                    data-aos-delay="100"
-                >
-                    CADHAC surge como respuesta a una realidad urgente:
-                    la necesidad de un espacio donde las personas
-                    pudieran encontrar
-                    <strong
-                        >acompañamiento, orientación y fuerza
-                        colectiva</strong
+                <?php if (!empty($texto_superior)): ?>
+                    <h1
+                        class="lh-1"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
                     >
-                    frente a la violencia y la impunidad.
-                </p>
-                <p
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                    data-aos-delay="200"
-                >
-                    Lo que comenzó como un esfuerzo ciudadano, impulsado
-                    por el compromiso y la solidaridad, fue creciendo
-                    hasta convertirse en
-                    <strong
-                        >una organización que acompaña, documenta y
-                        visibiliza</strong
-                    >, siempre desde una perspectiva humana y cercana.
-                </p>
+                        <?php echo wp_kses_post($texto_superior); ?>
+                    </h1>
+                <?php endif; ?>
+
+                <?php if (!empty($texto_inferior)): ?>
+                    <div
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                        data-aos-delay="100"
+                    >
+                        <?php echo wp_kses_post($texto_inferior); ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
+
         <div class="row">
+            <!-- Imagen -->
             <div class="col-lg-6">
-                <div
-                    class="arrow-right home-image container-image bp-center rounded-5 mb-5 mb-lg-0 p-4"
-                    style="
-                        background-image: url(&quot;<?php echo esc_url(
-                            get_template_directory_uri(),
-                        ); ?>/assets/images/bg-acompanamiento.png&quot;);
-                    "
-                    data-aos="fade-up"
-                    data-aos="fade-up"
-                    data-aos-duration="1000"
-                ></div>
+                <?php if (!empty($imagen_bloque)): ?>
+                    <div
+                        class="arrow-right home-image container-image bp-center rounded-5 mb-5 mb-lg-0 p-4"
+                        style="background-image:url('<?php echo esc_url(
+                            $imagen_bloque,
+                        ); ?>');"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                    ></div>
+                <?php endif; ?>
             </div>
+
+            <!-- Cards Stack -->
             <div class="col-lg-6">
                 <div class="cards-stack" id="mvvStack">
                     <div class="cards-stack-indicator">
                         👆 Toca cada tarjeta para ver más
                     </div>
-                    <div
-                        class="card card-ico-left rounded-5 p-4 cards-stack__card"
-                        data-index="0"
-                    >
-                        <div>
-                            <div class="row">
-                                <div class="col-6 my-auto">
-                                    <h1 class="card-title">Misión</h1>
-                                </div>
-                                <div class="col-6 my-auto text-end">
-                                    <img
-                                        src="<?php echo esc_url(
-                                            get_template_directory_uri(),
-                                        ); ?>/assets/images/mvv/ico-1@2x.png"
-                                        class="ico"
-                                        alt=""
-                                    />
-                                </div>
-                            </div>
 
-                            <div class="card-body">
-                                <p>
-                                    Acompañar a víctimas de violaciones
-                                    a derechos humanos, fortaleciendo
-                                    sus capacidades y promoviendo el
-                                    acceso a la verdad, la justicia y la
-                                    dignidad.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    <!-- Misión -->
+                    <?php if ($mision): ?>
+                        <div
+                            class="card card-ico-left rounded-5 p-4 cards-stack__card"
+                            data-index="0"
+                        >
+                            <div>
 
-                    <div
-                        class="card card-ico-left rounded-5 p-4 cards-stack__card"
-                        data-index="1"
-                    >
-                        <div>
-                            <div class="row">
-                                <div class="col-6 my-auto">
-                                    <h1 class="card-title">Visión</h1>
-                                </div>
-                                <div class="col-6 my-auto text-end">
-                                    <img
-                                        src="<?php echo esc_url(
-                                            get_template_directory_uri(),
-                                        ); ?>/assets/images/mvv/ico-2@2x.png"
-                                        class="ico"
-                                        alt=""
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="card-body">
-                                <p>
-                                    Una sociedad más justa, donde las
-                                    personas vivan sin miedo y con pleno
-                                    respeto a sus derechos.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div
-                        class="card card-ico-left rounded-5 p-4 cards-stack__card"
-                        data-index="2"
-                    >
-                        <div>
-                            <div class="row">
-                                <div class="col-6 my-auto">
-                                    <h1 class="card-title">Valores</h1>
-                                </div>
-                                <div class="col-6 my-auto text-end">
-                                    <img
-                                        src="<?php echo esc_url(
-                                            get_template_directory_uri(),
-                                        ); ?>/assets/images/mvv/ico-3@2x.png"
-                                        class="ico"
-                                        alt=""
-                                    />
-                                </div>
-                            </div>
-
-                            <div class="card-body">
                                 <div class="row">
-                                    <div class="col-6">
-                                        <ul>
-                                            <li>Dignidad humana</li>
-                                            <li>Empatía</li>
-                                            <li>Solidaridad</li>
-                                        </ul>
+                                    <div class="col-6 my-auto">
+                                        <h1 class="card-title">Misión</h1>
                                     </div>
-                                    <div class="col-6">
-                                        <ul>
-                                            <li>Justicia</li>
-                                            <li>Memoria</li>
-                                            <li>Trabajo colectivo</li>
-                                        </ul>
+
+                                    <div class="col-6 my-auto text-end">
+                                        <?php if (!empty($mision["icono"])): ?>
+                                            <img
+                                                src="<?php echo esc_url(
+                                                    $mision["icono"],
+                                                ); ?>"
+                                                class="ico"
+                                                alt="Misión"
+                                                loading="lazy"
+                                            />
+                                        <?php endif; ?>
                                     </div>
                                 </div>
+
+                                <?php if (!empty($mision["texto"])): ?>
+                                    <div class="card-body">
+                                        <?php echo wp_kses_post(
+                                            $mision["texto"],
+                                        ); ?>
+                                    </div>
+                                <?php endif; ?>
+
                             </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
+
+                    <!-- Visión -->
+                    <?php if ($vision): ?>
+                        <div
+                            class="card card-ico-left rounded-5 p-4 cards-stack__card"
+                            data-index="1"
+                        >
+                            <div>
+
+                                <div class="row">
+                                    <div class="col-6 my-auto">
+                                        <h1 class="card-title">Visión</h1>
+                                    </div>
+
+                                    <div class="col-6 my-auto text-end">
+                                        <?php if (!empty($vision["icono"])): ?>
+                                            <img
+                                                src="<?php echo esc_url(
+                                                    $vision["icono"],
+                                                ); ?>"
+                                                class="ico"
+                                                alt="Visión"
+                                                loading="lazy"
+                                            />
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+
+                                <?php if (!empty($vision["texto"])): ?>
+                                    <div class="card-body">
+                                        <?php echo wp_kses_post(
+                                            $vision["texto"],
+                                        ); ?>
+                                    </div>
+                                <?php endif; ?>
+
+                            </div>
+                        </div>
+                    <?php endif; ?>
+
+                    <!-- Valores -->
+                    <?php if ($valores): ?>
+                        <div
+                            class="card card-ico-left rounded-5 p-4 cards-stack__card"
+                            data-index="2"
+                        >
+                            <div>
+                                <div class="row">
+                                    <div class="col-6 my-auto">
+                                        <h1 class="card-title">Valores</h1>
+                                    </div>
+
+                                    <div class="col-6 my-auto text-end">
+                                        <?php if (!empty($valores["icono"])): ?>
+                                            <img
+                                                src="<?php echo esc_url(
+                                                    $valores["icono"],
+                                                ); ?>"
+                                                class="ico"
+                                                alt="Valores"
+                                                loading="lazy"
+                                            />
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+
+                                <?php if (!empty($valores["texto"])): ?>
+                                    <div class="card-body">
+                                        <?php echo wp_kses_post(
+                                            $valores["texto"],
+                                        ); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
