70154c15b16e2733865d64e547d10b4a8a6d0a49
Luis Carlos Pando <hey@luiscarlospando.com>
2026-02-11 15:47:53 -0600

    Armar page-historia

---
+++ page-historia.php
@@ -0,0 +1,266 @@
+<?php
+/*
+Template Name: Historia
+*/
+get_header(); ?>
+
+<?php get_template_part("template-parts/banners/banner-options", null, [
+    "handle" => "pagina_historia",
+]); ?>
+
+<section class="pt-60 pb-30">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <h1
+                    class="lh-1"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                >
+                    El inicio de un<br />
+                    acompañamiento <span>colectivo.</span>
+                </h1>
+                <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"
+                >
+                    CADHAC surge como respuesta a una realidad urgente:
+                    la necesidad de un espacio donde las personas
+                    pudieran encontrar
+                    <strong
+                        >acompañamiento, orientación y fuerza
+                        colectiva</strong
+                    >
+                    frente a la violencia y la impunidad.
+                </p>
+                <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="200"
+                >
+                    Lo que comenzó como un esfuerzo ciudadano, impulsado
+                    por el compromiso y la solidaridad, fue creciendo
+                    hasta convertirse en
+                    <strong
+                        >una organización que acompaña, documenta y
+                        visibiliza</strong
+                    >, siempre desde una perspectiva humana y cercana.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div
+                    class="arrow-right home-image container-image bp-center rounded-5 mb-5 mb-lg-0 p-4"
+                    style="
+                        background-image: url(&quot;<?php echo esc_url(
+                            get_template_directory_uri(),
+                        ); ?>/assets/images/bg-acompanamiento.png&quot;);
+                    "
+                    data-aos="fade-up"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                ></div>
+            </div>
+            <div class="col-lg-6">
+                <div class="cards-stack" id="mvvStack">
+                    <div class="cards-stack-indicator">
+                        👆 Toca cada tarjeta para ver más
+                    </div>
+                    <div
+                        class="card card-ico-left rounded-5 p-4 cards-stack__card"
+                        data-index="0"
+                    >
+                        <div>
+                            <div class="row">
+                                <div class="col-6 my-auto">
+                                    <h1 class="card-title">Misión</h1>
+                                </div>
+                                <div class="col-6 my-auto text-end">
+                                    <img
+                                        src="<?php echo esc_url(
+                                            get_template_directory_uri(),
+                                        ); ?>/assets/images/mvv/ico-1@2x.png"
+                                        class="ico"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <p>
+                                    Acompañar a víctimas de violaciones
+                                    a derechos humanos, fortaleciendo
+                                    sus capacidades y promoviendo el
+                                    acceso a la verdad, la justicia y la
+                                    dignidad.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class="card card-ico-left rounded-5 p-4 cards-stack__card"
+                        data-index="1"
+                    >
+                        <div>
+                            <div class="row">
+                                <div class="col-6 my-auto">
+                                    <h1 class="card-title">Visión</h1>
+                                </div>
+                                <div class="col-6 my-auto text-end">
+                                    <img
+                                        src="<?php echo esc_url(
+                                            get_template_directory_uri(),
+                                        ); ?>/assets/images/mvv/ico-2@2x.png"
+                                        class="ico"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <p>
+                                    Una sociedad más justa, donde las
+                                    personas vivan sin miedo y con pleno
+                                    respeto a sus derechos.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        class="card card-ico-left rounded-5 p-4 cards-stack__card"
+                        data-index="2"
+                    >
+                        <div>
+                            <div class="row">
+                                <div class="col-6 my-auto">
+                                    <h1 class="card-title">Valores</h1>
+                                </div>
+                                <div class="col-6 my-auto text-end">
+                                    <img
+                                        src="<?php echo esc_url(
+                                            get_template_directory_uri(),
+                                        ); ?>/assets/images/mvv/ico-3@2x.png"
+                                        class="ico"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <ul>
+                                            <li>Dignidad humana</li>
+                                            <li>Empatía</li>
+                                            <li>Solidaridad</li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-6">
+                                        <ul>
+                                            <li>Justicia</li>
+                                            <li>Memoria</li>
+                                            <li>Trabajo colectivo</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-60 pb-30">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <h1
+                    class="lh-1"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                >
+                    Más de 30<br />
+                    años <span>acompañando</span>
+                </h1>
+                <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="100"
+                >
+                    A lo largo de los años, CADHAC ha caminado junto a
+                    personas y comunidades en momentos difíciles,
+                    <strong
+                        >apostando siempre por la escucha, el respeto y
+                        la acción</strong
+                    >.
+                </p>
+                <p
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="200"
+                >
+                    Nuestro trabajo se ha transformado con el tiempo,
+                    pero el corazón sigue siendo el mismo: estar
+                    presentes, acompañar y no soltar.
+                </p>
+                <a
+                    class="btn btn-primary rounded-pill"
+                    href="#"
+                    download
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="300"
+                >
+                    <i class="fa-solid fa-circle-arrow-down"></i>
+                    Descargar Memoria CADHAC - 30 años</a
+                >
+            </div>
+            <div class="col-lg-6">
+                <div
+                    class="arrow-right home-image container-image bp-center rounded-5 p-4"
+                    style="
+                        background-image: url(&quot;<?php echo esc_url(
+                            get_template_directory_uri(),
+                        ); ?>/assets/images/bg-acompanamiento-2.png&quot;);
+                    "
+                    data-aos="fade-up"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                ></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 text-center">
+                <div
+                    class="bottom-text-image container-image bp-center rounded-5 p-4"
+                    style="
+                        background-image: url(&quot;<?php echo esc_url(
+                            get_template_directory_uri(),
+                        ); ?>/assets/images/bg-j-4.png&quot;);
+                    "
+                    data-aos="fade-up"
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-delay="200"
+                >
+                    <h1>
+                        Nuestra historia continúa, junto a las personas.
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
