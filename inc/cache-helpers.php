f6a85f6e8a71f37752e5d8665f3f5b7a51b7d364
Luis Carlos Pando <hey@luiscarlospando.com>
2026-02-11 14:42:09 -0600

    Agregar helper para limpiar caché

--- inc/cache-helpers.php
+++ inc/cache-helpers.php
@@ -1,5 +1,18 @@
 <?php
-add_action("save_post", function () {
+add_action("save_post", function ($post_id) {
+    // Evitar autosaves / revisions
+    if (wp_is_post_revision($post_id)) {
+        return;
+    }
+    if (wp_is_post_autosave($post_id)) {
+        return;
+    }
+
+    // Solo posts del blog
+    if (get_post_type($post_id) !== "post") {
+        return;
+    }
+
     $cats = [34, 86, 92, 12, 10, 11];
 
     foreach ($cats as $cat_id) {
