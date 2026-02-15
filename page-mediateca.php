0744d89aa665a87d934d7e215567a7cb767a0323
Luis Carlos Pando <hey@luiscarlospando.com>
2026-02-15 16:41:37 -0600

    Agregar page-mediateca

--- page-mediateca.php
+++ page-mediateca.php
@@ -4,8 +4,8 @@ Template Name: Mediateca
 */
 get_header(); ?>
 
-
-
-
+<?php get_template_part("template-parts/banners/banner-options", null, [
+    "handle" => "pagina_mediateca,
+]); ?>
 
 <?php get_footer(); ?>
